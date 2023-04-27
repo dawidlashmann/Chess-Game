577f488cce2f37efb621d8678e19253404b918d2
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-04-27 13:46:10 +0200

    simplify main

--- main.cpp
+++ main.cpp
@@ -5,71 +5,8 @@
 
 int main()
 {
-    bool round;
-    do
-    {
-        {
-            bool mode;
-            game *game_;
-            engine *ai_player;
-            std::cout << "Duo mode - 0\nSingle player mode - 1\n";
-            std::cin >> mode;
-            while (std::cin.fail())
-            {
-                std::cout << "Wrong input\nTry again\n";
-                std::cin.clear();
-                std::cin.ignore();
-                std::cin >> mode;
-                system("clear");
-                // system("cls");
-            }
-            if (mode)
-            {
-                system("clear");
-                // system("cls");
-                color side;
-                std::cout << "What color do you want to play?\n"
-                          << "1 - White\n2 - Black\n";
-                std::cin >> side;
-                while (std::cin.fail())
-                {
-                    std::cout << "Wrong input\nTry again\n";
-                    std::cin.clear();
-                    std::cin.ignore();
-                    std::cin >> side;
-                    system("clear");
-                    // system("cls");
-                }
-                ai_player = new engine((side == white) ? black : white);
-                game_ = ai_player;
-            }
-            else
-            {
-                game_ = new game();
-            }
-            game_->begin();
-            delete game_;
-            delete ai_player;
-        }
-        std::cout << "\nDO YOU WISH TO PLAY ANOTHER ROUND?" << '\n'
-                  << "1 - yes" << '\n'
-                  << "0 - no" << '\n';
+    game game_;
+    game_.begin();
 
-        std::cin >> round;
-
-        while (std::cin.fail())
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            system("clear");
-            // system("cls");
-            std::cout << "Wrong input!" << '\n'
-                      << "Try again" << '\n';
-            std::cout << "DO YOU WISH TO PLAY ANOTHER ROUND?" << '\n'
-                      << "1 - yes" << '\n'
-                      << "0 - no" << '\n';
-            std::cin >> round;
-        }
-
-    } while (round);
+    return 0;
 }
