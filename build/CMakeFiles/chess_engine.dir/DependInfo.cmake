58ceac06e1b40cebb4a1caca7d2dc7b89e21bb5d
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-07-15 00:51:10 +0200

    add CMake and build dir

---
+++ build/CMakeFiles/chess_engine.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dawid/repos/chess2/engine/engine.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/engine/engine.cpp.o"
+  "/home/dawid/repos/chess2/game_system/board.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/game_system/board.cpp.o"
+  "/home/dawid/repos/chess2/game_system/game.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/game_system/game.cpp.o"
+  "/home/dawid/repos/chess2/gui/window.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/gui/window.cpp.o"
+  "/home/dawid/repos/chess2/main.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/main.cpp.o"
+  "/home/dawid/repos/chess2/pieces/bishop.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/bishop.cpp.o"
+  "/home/dawid/repos/chess2/pieces/king.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/king.cpp.o"
+  "/home/dawid/repos/chess2/pieces/knight.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/knight.cpp.o"
+  "/home/dawid/repos/chess2/pieces/pawn.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/pawn.cpp.o"
+  "/home/dawid/repos/chess2/pieces/piece.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/piece.cpp.o"
+  "/home/dawid/repos/chess2/pieces/queen.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/queen.cpp.o"
+  "/home/dawid/repos/chess2/pieces/rook.cpp" "/home/dawid/repos/chess2/build/CMakeFiles/chess_engine.dir/pieces/rook.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../engine"
+  "../game_system"
+  "../gui"
+  "../pieces"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/chess_engine.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_engine.dir/engine/engine.cpp.o"
+  "CMakeFiles/chess_engine.dir/game_system/board.cpp.o"
+  "CMakeFiles/chess_engine.dir/game_system/game.cpp.o"
+  "CMakeFiles/chess_engine.dir/gui/window.cpp.o"
+  "CMakeFiles/chess_engine.dir/main.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/bishop.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/king.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/knight.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/pawn.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/piece.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/queen.cpp.o"
+  "CMakeFiles/chess_engine.dir/pieces/rook.cpp.o"
+  "chess_engine"
+  "chess_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
